2705d33699b3db78553398ed7514ff1b5b37b48b
MoikeCheck <mikewheatley@hotmail.co.uk>
2022-05-12 23:05:05 +0100

    comments removed

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
 const express = require("express");
 const server = express();
 const apiRouter = require("./routes/api");
-// const bodyParser = require("body-parser");
 
-// server.use(bodyParser.json());
-// server.use(
-//   bodyParser.urlencoded({
-//     extended: true,
-//   })
-// );
-// server.use(express.json());
 server.use("/api", apiRouter);
 
 module.exports = server;
